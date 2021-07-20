7c4ad9b048f340a039febb44aab4e0f462c27628
Jamile Conceição <jamile1008@gmail.com>
2021-07-20 18:05:28 -0300

    start OOP js

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,34 @@
-let inputResultado = document.getElementById('inputDisplayResultado')
+class Calculadora {
+  inputResultado = document.getElementById('inputDisplayResultado').value
 
-let calculation = {
-  firstValue: 0,
-  secondValue: 0,
-  calculator: null
-}
-
-// Adicionar evento para iniciar quando a janela carregar
-window.addEventListener('load', function () {
-  atribuirEventos()
-})
-
-// Atribuir eventos para os botões da calculadora
-function atribuirEventos() {
-  for (let index = 0; index <= 9; index++) {
-    document
-      .getElementById('btnValor' + index)
-      .addEventListener('click', inserirNumero)
+  calculation = {
+    firstValue: 0,
+    secondValue: 0,
+    calculator: null
   }
-}
 
-// Inserir um número no display da calculadora
-function inserirNumero() {
-  if (!isNaN(inputResultado.value)) {
-    if (inputResultado.value == 0) {
-      inputResultado.value = event.target.innerText
+  constructor() {
+    // Adicionar evento para iniciar quando a janela carregar
+    window.addEventListener('load', this.atribuirEventos())
+  }
+
+  // Atribuir eventos para os botões da calculadora
+  atribuirEventos() {
+    for (let index = 0; index <= 9; index++) {
+      document
+        .getElementById('btnValor' + index)
+        .addEventListener('click', this.inserirNumero)
+    }
+  }
+
+  // Inserir um número no display da calculadora
+  inserirNumero() {
+    if (isNaN(this.inputResultado) || this.inputResultado == 0) {
+      this.inputResultado = event.target.innerText
     } else {
-      inputResultado.value += event.target.innerText
+      this.inputResultado += event.target.innerText
     }
   }
 }
+
+new Calculadora()
