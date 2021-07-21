211c7b799955c66d1b4db23f44cbf37f9ab3f3df
Jamile Conceição <jamile1008@gmail.com>
2021-07-21 16:00:29 -0300

    operacoes, ponto e limpar input

--- scripts/main.js
+++ scripts/main.js
@@ -14,11 +14,22 @@ class Calculadora {
 
   // Atribuir eventos para os botões da calculadora
   atribuirEventos = () => {
+    // Atribuir eventos para os botões de número de 0 a 9
     for (let index = 0; index <= 9; index++) {
       document
         .getElementById('btnValor' + index)
         .addEventListener('click', this.inserirNumero)
     }
+
+    // Atribuir evento para o botão C que limpa dos dados da calculadora
+    document
+      .getElementById('btnLimpar')
+      .addEventListener('click', this.limparDados)
+
+    // Atribuir evento ao botão de .
+    document
+      .getElementById('btnPonto')
+      .addEventListener('click', this.inserirPonto)
   }
 
   // Inserir um número no display da calculadora
@@ -29,6 +40,32 @@ class Calculadora {
       this.inputResultado.value += event.target.innerText
     }
   }
+
+  // Funções com operações matemática
+  somar = (valor1, valor2) => valor1 + valor2
+
+  subtracao = (valor1, valor2) => valor1 - valor2
+
+  divisao = (valor1, valor2) => (valor2 != 0 ? valor1 / valor2 : undefined)
+
+  multiplicacao = (valor1, valor2) => valor1 * valor2
+
+  // Função para limpar dados da calculadora
+  limparDados = () => {
+    this.inputResultado.value = ''
+    this.calculation.firstValue = 0
+    this.calculation.secondValue = 0
+    this.calculation.calculator = null
+  }
+
+  // Função para inserir o ponto
+  inserirPonto = () => {
+    if (this.inputResultado.value === '' || isNaN(this.inputResultado.value)) {
+      this.inputResultado.value = '0.'
+    } else if (!this.inputResultado.value.includes('.')) {
+      this.inputResultado.value += '.'
+    }
+  }
 }
 
-new Calculadora()
+calculadora = new Calculadora()
